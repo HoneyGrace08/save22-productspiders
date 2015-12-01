4c0f105778e453f81ef5d15d5e7a367bb14716bf
honeygrace <honeygrace0508@gmail.com>
2015-12-01 04:25:25 -0500

    Initialized expansys spiders.

---
+++ save22_productspiders/spiders/www_expansys_com_sg_crawler.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from scrapy import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from save22_productspiders.items import Save22ProductspidersItem
+
+class WwwExpansysComSgCrawler ():
+  name = "www_expansys_com_sg_crawler"
+  allowed_domains = ["expansys.com.sg"]
+  start_urls = [
+        "http://www.expansys.com.sg/mobile-phones/",
+    ]
+  
+  rules = (
+    Rule (LinkExtractor(
+          allow = (r'(.+)',),
+          callback = 'parse_item',
+          )),
+  )
+
+  def parse_item(self, response):
+    pass
